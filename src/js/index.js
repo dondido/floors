30daa4cd120c513b81ef62af4735429c2ddcb3e8
didosaurus <dian_t_d@yahoo.co.uk>
2020-02-08 07:39:31 +0000

    Added pane dragging

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,11 @@
-let plan;
+let plan, $pane;
 const handleJson = response => response.json();
 const handleText = response => response.text();
 const $views = document.querySelector('.views');
 const $floorSelector = document.querySelector('.floor-selector');
 const $zoomSlider = document.querySelector('.zoom-slider');
+const $viewFragment = document.createDocumentFragment();
+const $floorFragment = document.createDocumentFragment();
 const views = [];
 const floorOptions = [];
 const hideNode = node => node.hidden = true;
@@ -14,46 +16,75 @@ const selectFloor = (e) => {
         floorOptions.forEach(hideNode);
         views.forEach(hideNode);
         target.hidden = false;
-        document.getElementById(target.dataset.ref).hidden = false;
+        $pane = document.getElementById(target.dataset.ref);
+        $pane.hidden = false;
     }
 };
 const zoom = () => document.body.style.setProperty('--custom-scale', $zoomSlider.value);
+const setFloor = (floor, idx) => {
+    const $view = document.createElement('div');
+    const $floorOption = document.createElement('li');
+    const appendSvg = text => {
+        $view.innerHTML = text;
+        const $svg = $view.querySelector('svg');
+        const { width, height } = $svg.getBoundingClientRect();
+        const scale = Math.min(window.innerWidth / width, window.innerHeight / height);
+        $svg.style.setProperty('--default-scale', scale);
+    };
+    $floorOption.textContent = floor.name;
+    $floorOption.className = 'floor-option';
+    floorOptions.push($floorOption);
+    $view.hidden = $floorOption.hidden = true;
+    $floorOption.dataset.ref = $view.id = `view${idx}`;
+    $floorFragment.appendChild($floorOption);
+    $view.className = 'view';
+    views.push($view);
+    $viewFragment.appendChild($view);
+
+    fetch(floor.src)
+        .then(handleText)
+        .then(appendSvg)
+};
+const dragNode = (node) => {
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    const end = () => {
+        /* stop moving when mouse button is released:*/
+        document.onmouseup = null;
+        document.onmousemove = null;
+    };
+    const drag = (e) => {
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        // set the element's new position:
+        node.style.setProperty('--x', `${node.offsetLeft - pos1}px`);
+        node.style.setProperty('--y', `${node.offsetTop - pos2}px`);
+    };
+    const start = (e) => {
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX - parseInt(getComputedStyle(node).getPropertyValue('--x'));
+        pos4 = e.clientY - parseInt(getComputedStyle(node).getPropertyValue('--y'));
+        document.onmouseup = end;
+        // call a function whenever the cursor moves:
+        document.onmousemove = drag;
+    };
+    $views.onmousedown = start;
+};
 const loadPlan = (raw) => {
-    const $viewFragment = document.createDocumentFragment();
-    const $floorFragment = document.createDocumentFragment();
     plan = raw;
-    plan.forEach((floor, idx) => {
-        const $view = document.createElement('div');
-        const $floorOption = document.createElement('li');
-        const appendSvg = text => {
-            $view.innerHTML = text;
-            const $svg = $view.querySelector('svg');
-            const { width, height } = $svg.getBoundingClientRect();
-            const scale = Math.min(window.innerWidth/width, window.innerHeight/height);
-            $svg.style.setProperty('--default-scale', scale);
-        };
-        $floorOption.textContent = floor.name;
-        $floorOption.className = 'floor-option';
-        floorOptions.push($floorOption);
-        $view.hidden = $floorOption.hidden = true;
-        $floorOption.dataset.ref = $view.id = `view${idx}`;
-        $floorFragment.appendChild($floorOption);
-        $view.className = 'view';
-        views.push($view);
-        $viewFragment.appendChild($view);
-        
-        fetch(floor.src)
-            .then(handleText)
-            .then(appendSvg)
-    });
+    plan.forEach(setFloor);
     $views.appendChild($viewFragment);
     $floorSelector.appendChild($floorFragment);
+    $pane = document.querySelector('.view');
     document.querySelector('.floor-option').hidden = false;
-    document.querySelector('.view').hidden = false;
+    $pane.hidden = false;
+    dragNode($pane);
     $floorSelector.addEventListener('click', selectFloor)
     $zoomSlider.addEventListener('input', zoom);
 };
 
 fetch('plan.json')
-  .then(handleJson)
-  .then(loadPlan);
+    .then(handleJson)
+    .then(loadPlan);
