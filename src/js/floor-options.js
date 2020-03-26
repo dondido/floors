c701f7d2e7b125f2e37f393bb593403a8225be73
didosaurus <dian_t_d@yahoo.co.uk>
2020-03-26 19:43:29 +0000

    Added floor option info

--- src/js/floor-options.js
+++ src/js/floor-options.js
@@ -1,26 +1,33 @@
 import inject from './index.js';
 
-
-const $floors = document.querySelector('.floor-option-template').content.firstElementChild.cloneNode(true);
+const floorMap = {};
+const $floors = document.querySelector('.floor-option-template').content.firstElementChild;
+const $floorOptionItem = document.querySelector('.floor-option-item-template').content.firstElementChild;
+const $floorOptionInfo = document.querySelector('.floor-option-info-template').content.firstElementChild;
 const $floorList = document.querySelector('.floor-list');
+const setDisabled = (acc, id) => `${acc}<li>${floorMap[id]}</li>`;
+const mapNameToId = (acc, { id, name }) => Object.assign(acc, { [id]: name });;
 
-const setFloor = ({name, id, options}, idx) => {
+const setFloor = ({name, id, options}) => {
     if (options === undefined) {
         return;
     }
     const $floor = $floors.cloneNode(true);
     const $floorBody = $floor.querySelector('.floor-item-body');
-    const setOption = option => {
-        const $floorOption = document.createElement('li');
-        $floorOption.textContent = option.name;
+    const setOption = ({ name, disable }) => {
+        const $floorOption = $floorOptionItem.cloneNode(true);
+        $floorOption.firstElementChild.textContent = name;
         $floorBody.appendChild($floorOption);
+        if (disable) {
+            const $floorInfo = $floorOptionInfo.cloneNode(true);
+            $floorInfo.querySelector('.floor-option-info-list').innerHTML = disable.reduce(setDisabled, '');
+            $floorBody.appendChild($floorInfo);
+        }
     };
-    
-   
+    Object.assign(floorMap, options.reduce(mapNameToId, { [id]: name }));
     $floor.querySelector('.floor-item-summary').textContent = name;
     options.forEach(setOption);
     $floorList.appendChild($floor);
-    
 };
 
 const setFloorOptions = (plan) => {
