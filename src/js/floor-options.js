1b85569693ba82b53612df97459fabb165d64693
didosaurus <dian_t_d@yahoo.co.uk>
2020-03-27 19:45:10 +0000

    Added furniture option button click

--- src/js/floor-options.js
+++ src/js/floor-options.js
@@ -6,25 +6,49 @@ const $floorOptionItem = document.querySelector('.floor-option-item-template').c
 const $floorOptionInfo = document.querySelector('.floor-option-info-template').content.firstElementChild;
 const $floorList = document.querySelector('.floor-list');
 const setDisabled = (acc, id) => `${acc}<li>${floorMap[id]}</li>`;
-const mapNameToId = (acc, { id, name }) => Object.assign(acc, { [id]: name });;
+const mapNameToId = (acc, i) => Object.assign(acc, { [i.id]: i });
 
-const setFloor = ({name, id, options}) => {
+const enableOption = ({ target }) => {
+    const checked = target.getAttribute('aria-checked') !== 'true';
+    const $floorBody = target.closest('.floor-item-body');
+    const { id } = target.dataset;
+    console.log(123, id, floorMap[id])
+    const { disable = [] } = floorMap[id];
+    const disableOthers = (ref) => {
+        console.log(112, ref)
+        const $target = $floorBody.querySelector(`[data-id="${ref}"]`);
+        $target.dataset.disabled ++;
+    };
+    target.setAttribute('aria-checked', checked);
+    document.getElementById(id).setAttribute('display', checked ? 'inline' : 'none');
+    disable.forEach(disableOthers);
+};
+
+const setFloor = (floor) => {
+    const {name, id, options} = floor;
     if (options === undefined) {
         return;
     }
     const $floor = $floors.cloneNode(true);
     const $floorBody = $floor.querySelector('.floor-item-body');
-    const setOption = ({ name, disable }) => {
+    const setOption = ({ name, id, disable, disabled = 0 }) => {
         const $floorOption = $floorOptionItem.cloneNode(true);
-        $floorOption.firstElementChild.textContent = name;
+        const $button = $floorOption.firstElementChild;
+        $button.textContent = name;
+        $button.dataset.id = id;
+        $button.dataset.disabled = disabled;
+        $button.onclick = enableOption;
         $floorBody.appendChild($floorOption);
         if (disable) {
             const $floorInfo = $floorOptionInfo.cloneNode(true);
+            const $subButton = $floorInfo.querySelector('.floor-extra-option-button');
             $floorInfo.querySelector('.floor-option-info-list').innerHTML = disable.reduce(setDisabled, '');
+            $subButton.dataset.id = id;
+            $subButton.onclick = () => enableOption({ target: $button });
             $floorOption.appendChild($floorInfo);
         }
     };
-    Object.assign(floorMap, options.reduce(mapNameToId, { [id]: name }));
+    Object.assign(floorMap, options.reduce(mapNameToId, { [floor.id]: floor }));
     $floor.querySelector('.floor-item-summary').textContent = name;
     options.forEach(setOption);
     $floorList.appendChild($floor);
